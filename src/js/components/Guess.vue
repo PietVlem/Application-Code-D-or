b866f6aaee695d245fff433fcc49d85065fb27f8
Pieter <pieter.vlem@gmail.com>
2022-05-20 12:28:36 +0200

    [Wip] button color-select popover 😩

--- src/js/components/Guess.vue
+++ src/js/components/Guess.vue
@@ -1,39 +1,72 @@
 <template>
-  <form class="play-field" :class="{'play-field--guess': !editable}">
+  <form class="play-field" :class="{ 'play-field--guess': !editable }">
     <ol class="play-field__color-select">
       <li v-for="(color, index) in selectedColors" :key="index">
         <template v-if="editable">
-          <button 
-              class="button button--game" 
-              type="button" 
-              @click="$event => showOptions = index">
+          <button class="button button--game" type="button" @click="$event => showOptions = index">
             <Color :color="color" />
           </button>
 
           <fieldset class="color-options" v-if="showOptions === index">
             <div v-for="colorOption in possibleColors">
-              <input hidden type="radio" name="color" :id="colorOption" :value="colorOption" @input="$event => chooseColor(index, colorOption)" />
+              <input hidden type="radio" name="color" :id="colorOption" :value="colorOption"
+                @input="$event => chooseColor(index, colorOption)" />
               <label :for="colorOption">
                 <Color :color="colorOption" />
               </label>
             </div>
             <div>
-              <input hidden type="radio" name="color" id="none" value="none" @input="$event => chooseColor(index, 'none')" />
+              <input hidden type="radio" name="color" id="none" value="none"
+                @input="$event => chooseColor(index, 'none')" />
               <label for="none">
                 <Color color="none">X</Color>
               </label>
             </div>
           </fieldset>
+
+          <!-- <button 
+              data-bs-toggle="popover" 
+              data-bs-placement="bottom" 
+              class="button button--game" 
+              type="button" 
+              @click="$event => showOptions = index">
+              {{ index }}
+            <Color :color="color" />
+          </button> -->
+
+          <!-- popover -->
+          <!-- <div style="display: none;" > 
+            <div data-name="popover-content">
+                <fieldset class="popover-content__inner-wrapper" v-if="showOptions === index">
+                  <div class="radio-color" v-for="colorOption in possibleColors">
+                    <input hidden type="radio" name="color" :id="colorOption" :value="colorOption" @input="$event => chooseColor(index, colorOption)" />
+                    <label :for="colorOption">
+                      <Color :color="colorOption" />
+                    </label>
+                  </div>
+                  <div>
+                    <input hidden type="radio" name="color" id="none" value="none" @input="$event => chooseColor(index, 'none')" />
+                    <label for="none">
+                      <Color color="none">
+                        <span>X</span>
+                      </Color>
+                    </label>
+                  </div>
+                </fieldset>
+            </div>
+          </div> -->
+          
         </template>
         <Color v-else :color="color" />
       </li>
     </ol>
-    <button v-if="editable" class="button button--primary"  type="submit" @click.prevent="guess">Guess</button>
+    <button v-if="editable" class="button button--primary" type="submit" @click.prevent="guess">Guess</button>
     <Pins v-else :pins="pins" />
   </form>
 </template>
 
 <script>
+import { Popover } from "bootstrap";
 import Color from './Color.vue'
 import Pins from './Pins.vue'
 
@@ -42,7 +75,7 @@ export default {
   components: { Color, Pins },
   props: {
     colors: {
-      type: [Array,Boolean],
+      type: [Array, Boolean],
       default: false
     }
   },
@@ -52,7 +85,18 @@ export default {
     showOptions: false,
     editable: true
   }),
-  mounted () {
+  mounted() {
+    // bootstrap popover
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    if (popoverTriggerList) {
+      const options = {
+        html: true,
+        content: document.querySelector('[data-name="popover-content"]'),
+        //trigger: 'focus'
+      }
+      const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl, options))
+    }
+
     // Get the possible colors from where we set them in the created() function in the App.vue
     this.possibleColors = window.possibleColors
     if (this.colors) {
@@ -62,7 +106,7 @@ export default {
     }
   },
   computed: {
-    pins () {
+    pins() {
       if (this.editable) {
         // If this row is an editable one, we can't see any pins
         return false
@@ -80,7 +124,7 @@ export default {
     }
   },
   methods: {
-    chooseColor (index, color) {
+    chooseColor(index, color) {
       // If the chosen color already exists in the code, we set the other instance of that color
       // to none, making the newly selected one the only instance of that color in the guess
       const currentIndex = this.selectedColors.findIndex(selected => color === selected)
@@ -92,7 +136,7 @@ export default {
       this.selectedColors[index] = color
       this.showOptions = false
     },
-    guess () {
+    guess() {
       this.editable = false
 
       // TODO - determine wether the guess is correct or not
